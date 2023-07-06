441cd493fb97e21f168b450029dbe4104c94aed3
asters1 <gj744559140@163.com>
2023-07-06 00:16:31 +0000

    lua

--- script/translator.sh
+++ script/translator.sh
@@ -2,15 +2,25 @@
 # $@是所有参数
 #cpu架构
 # echo $@ > command.txt
+IsExists () {
+  if [ -e "$1" ]; then
+    echo "$1 存在"
+  else
+    echo "$1 不存在"
+  fi
+}
 jg=$(uname -a | awk -F " " '{print $(NF-1)}')
 sys=$(uname -a | awk -F " " '{print $(NF)}')
 if [ $sys == "GNU/Linux" ];then
   if [ $jg == "aarch64" ];then
-~/.config/nvim/plugged/vim-fy/script/fy_aarch64 $@
+    # ~/.config/nvim/plugged/vim-fy/script/fy_aarch64 $@
+    ~/.local/share/nvim/lazy/vim-fy/script/fy_aarch64 $@
   else
-~/.config/nvim/plugged/vim-fy/script/fy_x86 $@
+    # ~/.config/nvim/plugged/vim-fy/script/fy_x86 $@
+    ~/.local/share/nvim/lazy/vim-fy/script/fy_x86 $@
   fi
 else
-~/.config/nvim/plugged/vim-fy/script/fy_andorid $@
+  # ~/.config/nvim/plugged/vim-fy/script/fy_android $@
+  ~/.local/share/nvim/lazy/vim-fy/script/fy_android $@
 fi
 
